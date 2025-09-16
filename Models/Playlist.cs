cbec91fbd4fa32636ef8c2e290d197d0a2471a03
Rugilė Alkovikaitė <rugilealk555@gmail.com>
2025-09-16 21:28:59 +0300

    Added new Playlist class with some basic functionality (add, delete songs, song count)

---
+++ Models/Playlist.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Playlist
+{
+	private List<Song> Songs;
+	public Playlist()
+	{
+		Songs = new List<Song>();
+	}
+	public void AddSong(Song newSong)
+	{
+		Songs.Add(newSong);
+	}
+	public List<Song> GetAllSongs()
+	{
+		return Songs.OrderByDescending(s => s.Votes).ToList();
+	}
+	public Song? GetSongById(Guid id)
+	{
+		return Songs.FirstOrDefault(s => s.Id == id);
+	}
+	public bool DeleteSong(Guid id)
+	{
+		var song = GetSongById(id);
+        if (song == null) return false;
+        return Songs.Remove(song);
+	}
+	public int ReturnCount()
+	{
+		return Songs.Count;
+    }
+}
