06ce2e44e5231eee09c96dadc12f23425636ac66
Rugilė Alkovikaitė <rugilealk555@gmail.com>
2025-09-16 21:37:55 +0300

    Added private setters and a constructor + Upvote() function

--- Models/Song.cs
+++ Models/Song.cs
@@ -2,8 +2,14 @@ namespace PSI.Models;
 
 public class Song
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public string Title { get; set; } = string.Empty;
-    public string Artist { get; set; } = string.Empty;
-    public int Votes { get; set; } = 0;
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Title { get; private set; }
+    public string Artist { get; private set; }
+    public int Votes { get; private set; } = 0;
+    public Song(string title, string artist)
+    {
+        Title = title;
+        Artist = artist;
+    }
+    public void Upvote() => Votes++;
 }
