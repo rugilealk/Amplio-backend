cbe394c9487caa4601adef73d4b5e7f959af145e
Rugilė Alkovikaitė <rugilealk555@gmail.com>
2025-09-16 22:06:32 +0300

    Added DeleteSong and ReturnCount functions, corrected Upvote function to work with the updated Song class

--- Services/PlaylistService.cs
+++ Services/PlaylistService.cs
@@ -18,12 +18,24 @@ public class PlaylistService
     {
         var s = _songs.FirstOrDefault(x => x.Id == id);
         if (s is null) return false;
-        s.Votes++;
+        s.Upvote();
         return true;
     }
 
     public Song? GetById(Guid id)
     {
         return _songs.FirstOrDefault(s => s.Id == id);
+    }
+
+    public bool DeleteSong(Guid id)
+    {
+        var song = GetById(id);
+        if (song == null) return false;
+        return _songs.Remove(song);
+    }
+
+    public int ReturnCount()
+    {
+        return _songs.Count;
     }
 }
