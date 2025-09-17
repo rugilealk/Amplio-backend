324656925871e5f0f702ff654809eaae5cc9e8b6
Rugilė Alkovikaitė <rugilealk555@gmail.com>
2025-09-17 11:11:38 +0300

    Added missing library and a new field called PlaylistId

--- Models/Playlist.cs
+++ Models/Playlist.cs
@@ -1,33 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class Playlist
+namespace PSI.Models;
 {
-	private List<Song> Songs;
-	public Playlist()
+	public class Playlist
 	{
-		Songs = new List<Song>();
-	}
-	public void AddSong(Song newSong)
-	{
-		Songs.Add(newSong);
-	}
-	public List<Song> GetAllSongs()
-	{
-		return Songs.OrderByDescending(s => s.Votes).ToList();
-	}
-	public Song? GetSongById(Guid id)
-	{
-		return Songs.FirstOrDefault(s => s.Id == id);
-	}
-	public bool DeleteSong(Guid id)
-	{
-		var song = GetSongById(id);
-        if (song == null) return false;
-        return Songs.Remove(song);
-	}
-	public int ReturnCount()
-	{
-		return Songs.Count;
-    }
+		private List<Song> Songs;
+		private readonly Guid PlaylistId;
+		public Playlist()
+		{
+			Songs = new List<Song>();
+			PlaylistId = Guid.NewGuid();
+		}
+		public void AddSong(Song newSong)
+		{
+			Songs.Add(newSong);
+		}
+		public List<Song> GetAllSongs()
+		{
+			return Songs.OrderByDescending(s => s.Votes).ToList();
+		}
+		public Song? GetSongById(Guid id)
+		{
+			return Songs.FirstOrDefault(s => s.Id == id);
+		}
+		public bool DeleteSong(Guid id)
+		{
+			var song = GetSongById(id);
+			if (song == null) return false;
+			return Songs.Remove(song);
+		}
+		public int ReturnCount()
+		{
+			return Songs.Count;
+		}
 
+	}
 }
