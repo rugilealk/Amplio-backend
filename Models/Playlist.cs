da9253de5a91882aaed4a0ce3dfcd49babaea519
Rugilė Alkovikaitė <rugilealk555@gmail.com>
2025-09-17 11:17:44 +0300

    Improved playlist shuffling logic: added upvoteSong function, SortSongs function and removed sorting in GetAllSongs function.

--- Models/Playlist.cs
+++ Models/Playlist.cs
@@ -7,33 +7,51 @@ namespace PSI.Models;
 	{
 		private List<Song> Songs;
 		private readonly Guid PlaylistId;
+
 		public Playlist()
 		{
 			Songs = new List<Song>();
 			PlaylistId = Guid.NewGuid();
 		}
+
 		public void AddSong(Song newSong)
 		{
 			Songs.Add(newSong);
 		}
-		public List<Song> GetAllSongs()
-		{
-			return Songs.OrderByDescending(s => s.Votes).ToList();
-		}
+
+		public List<Song> GetAllSongs() {  return Songs; }
+
 		public Song? GetSongById(Guid id)
 		{
 			return Songs.FirstOrDefault(s => s.Id == id);
 		}
+
 		public bool DeleteSong(Guid id)
 		{
 			var song = GetSongById(id);
 			if (song == null) return false;
 			return Songs.Remove(song);
 		}
+
 		public int ReturnCount()
 		{
 			return Songs.Count;
 		}
 
-	}
+		public void UpvoteSong(Guid id)
+        {
+            var song = GetSongById(id);
+            if (song != null)
+            {
+                song.Upvote();
+                SortSongs();
+            }
+        }
+
+		private void SortSongs()
+		{
+			Songs = Songs.OrderByDescending(s => s.Votes).ToList();
+		}
+
+}
 }
