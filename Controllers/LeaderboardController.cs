37427d788746fc64bd8d26c066acc2d0f61ac04e
Rugilė Alkovikaitė <rugilealk555@gmail.com>
2025-11-11 18:21:09 +0200

    Added leaderboard service and controller

---
+++ Controllers/LeaderboardController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PSI.Services;
+
+namespace PSI.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        private readonly LeaderboardService _leaderboardService;    
+        public LeaderboardController(LeaderboardService leaderboardService)
+        {
+            _leaderboardService = leaderboardService;
+        }
+
+        [HttpGet("playlists")]
+        public async Task<IActionResult> GetPlaylistLeaderboard([FromQuery] int topN = 10)
+        {
+            var leaderboard = await _leaderboardService.GetPlaylistLeaderboardAsync(topN);
+            return Ok(new
+            {
+                items = leaderboard.GetSortedByPopularity()
+            });
+        }
+
+        [HttpGet("albums")]
+        public async Task<IActionResult> GetAlbumLeaderboard([FromQuery] int topN = 10)
+        {
+            var leaderboard = await _leaderboardService.GetAlbumLeaderboardAsync(topN);
+            return Ok(new
+            {
+                items = leaderboard.GetSortedByPopularity()
+            });
+        }
+    }
+}
---
+++ Services/LeaderboardService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PSI.Data;
+using PSI.Models;
+
+namespace PSI.Services
+{
+    public class LeaderboardService
+    {
+        private readonly AppDbContext _databaseContext;
+
+        public LeaderboardService(AppDbContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public async Task<GenericLeaderboard<Playlist>> GetPlaylistLeaderboardAsync(int topN = 10)
+        {
+            var leaderboard = new GenericLeaderboard<Playlist>();
+
+            var playlists = await _databaseContext.Playlists
+                .Where(p => p.IsPublic)
+                .Include(p => p.Songs)
+                .ToListAsync();
+
+            foreach (var playlist in playlists)
+            {
+                leaderboard.AddSongCollection(playlist);
+            }
+
+            return leaderboard;
+        }
+        public async Task<GenericLeaderboard<Album>> GetAlbumLeaderboardAsync(int topN = 10)
+        {
+            var leaderboard = new GenericLeaderboard<Album>();
+
+            var albums = await _databaseContext.Albums
+                .Include(a => a.Songs)
+                .ToListAsync();
+
+            foreach (var album in albums)
+            {
+                leaderboard.AddSongCollection(album);
+            }
+            return leaderboard;
+        }
+    }
+}
