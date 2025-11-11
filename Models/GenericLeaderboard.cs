720b6cad29425a618a859c8944f947565fdd7ee3
Rugilė Alkovikaitė <rugilealk555@gmail.com>
2025-11-11 16:48:14 +0200

    Created GenericLeaderboard class

---
+++ Models/GenericLeaderboard.cs
@@ -0,0 +1,27 @@
+﻿namespace PSI.Models
+{
+    public class GenericLeaderboard<T> where T : SongCollection
+    {
+        public List<T> LeaderboardItems { get; set; } = new List<T>();
+        public void AddSongCollection(T collection)
+        {
+            if (collection != null && !LeaderboardItems.Any(c=>c.Id == collection.Id)) //gal dar padaryt, kad jei neturi elementu tai nepridesim 
+                LeaderboardItems.Add(collection);
+        }
+
+        public List<T> GetSortedByPopularity(int? numberOfSongs = null)
+        {
+            var sorted =  LeaderboardItems.OrderByDescending(c => c.Popularity).ThenBy(c => c.Name);
+            return numberOfSongs.HasValue ? sorted.Take(numberOfSongs.Value).ToList() : sorted.ToList();
+        }
+
+        public void RemoveSongList(Guid id)
+        {
+            var collection = LeaderboardItems.FirstOrDefault(c => c.Id == id);
+            if (collection != null)
+            {
+                LeaderboardItems.Remove(collection);
+            }
+        }
+    }
+}
