37034bef78f29d3ddfa79a1f19e1de3d48ab1213
Rugilė Alkovikaitė <rugilealk555@gmail.com>
2025-11-11 16:08:54 +0200

    Added SongCollection parent class

---
+++ Models/Album.cs
@@ -0,0 +1,6 @@
+﻿namespace PSI.Models
+{
+    public class Album
+    {
+    }
+}
---
+++ Models/SongCollection.cs
@@ -0,0 +1,13 @@
+﻿namespace PSI.Models
+{
+    public abstract class SongCollection
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int popularity { get; set; }
+        public SongCollection(string name)
+        {
+            Name = name;
+        }
+    }
+}
