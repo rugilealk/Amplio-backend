5107e29573a2147c09ea1b703fed19653174513c
DeimanteDav <deimante.davi@gmail.com>
2025-11-17 15:45:17 +0200

    feat(coverage): add code coverage support

--- PSI/Migrations/20251019112511_AddCurrentSongToPlaylist.cs
+++ PSI/Migrations/20251019112511_AddCurrentSongToPlaylist.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Diagnostics.CodeAnalysis;
 
 #nullable disable
 
 namespace PSI.Migrations
 {
     /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
     public partial class AddCurrentSongToPlaylist : Migration
     {
         /// <inheritdoc />
